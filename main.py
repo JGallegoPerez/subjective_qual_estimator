c340832313582a528fe363d7eab05d773fcf38c4
Jorge <jgallegopz@gmail.com>
2022-11-30 15:14:30 +0900

    Added class Session

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ preselect_dir = "images/dir" #This directory and its subdirectories are programa
 
 
 
-
 #Show random images from the images directory 
 
-
+session = sqe.Session()
+print("type of session.subs_dict: ", type(session.subs_dict))
 images_main = sqe.Subframe_group(images_main_dir)
-disp = sqe.Display(images_main, "practice")
-disp.run()
+disp1 = sqe.Display(session, images_main, "practice")
+disp1.run()
+disp2 = sqe.Display(session,  images_main, "preselect")
+disp2.run()
 
+
+print(session.subs_dict)
 
 #print(images_main.im_name_list)
 
--- sqe.py
+++ sqe.py
@@ -7,9 +7,12 @@ import time
 import matplotlib.pyplot as plt
 
 
-#The most important data structure. This dictionary contains the names of all images as keys, and the values are: pressed key, estimated quality and stacking weight.
-#The dictionary has entries added as the application progresses, so that it can be accessed anytime. 
-subs_dict = {}
+class Session:
+    def __init__(self):
+        #The most important data structure. This dictionary contains the names of all images as keys, and the values are: pressed key, estimated quality and stacking weight.
+        #The dictionary has entries added as the application progresses, so that it can be accessed anytime. 
+        self.subs_dict = {}
+
 
 
 class Subframe:
@@ -50,20 +53,22 @@ class Display:
     im_delay = 0.25
     num_subs_practice = 2
 
-    def __init__(self, subs_group, disp_type):
+    def __init__(self, session, subs_group, disp_type):
+        self.session = session
         self.subs_group = subs_group 
         self.disp_type = disp_type #Types: practice, preselect, rating
         if self.disp_type == "practice":
             self.instructions = f"You will see a series of subframes ({self.num_subs_practice}) as practice. Press any key to show the next image."
         elif self.disp_type == "preselect":
+            #self.session = session
             self.instructions = "Reject or select each image. Press <,> for rejecting and <.> for selecting."
         elif self.disp_type == "rating":
+            #self.session = session
             self.instructions = "Rate each image from 1 (worst quality) to 5 (best quality) by pressing 1-5 number keys on the keyboard."
         
 
 
     def run(self, random_order=True):
-        
         #Get the list of the names of the images
         im_names = self.subs_group.im_name_list
         if random_order:
@@ -72,9 +77,7 @@ class Display:
             else:
                 random.shuffle(im_names)
 
-
         print(self.instructions + "\n")
-
 
         if self.disp_type == "practice":
             counter = 0
@@ -99,13 +102,30 @@ class Display:
 
 
         elif self.disp_type == "preselect":
-            #Create new Subframe_group object? subdirectory "preselect...?"
-            pass
+            counter = 0
+            frame_include = False
+            while counter < 5: #test
+                im_name = im_names[counter]
+                im_path = os.path.join(self.subs_group.directory, im_name)
+                img = cv2.imread(im_path)
+                img = cv2.resize(img, self.disp_size)
+                cv2.imshow(im_path, img)
+                pressed_key = cv2.waitKey(0) & 0xFF
+                print(f"{counter + 1} subframes shown")   
+                cv2.destroyAllWindows()
+                time.sleep(self.im_delay)     
+                
+                if pressed_key == ord('q'):
+                    cv2.destroyAllWindows()
+                    print("Application terminated")
+                    break
+                elif pressed_key == ord(','):
+                    frame_include = False
+                    self.session.subs_dict[im_name] = [0]
 
-            # elif self.disp_type == "rating":
 
 
-            #     pass
+                counter += 1
 
 
 
